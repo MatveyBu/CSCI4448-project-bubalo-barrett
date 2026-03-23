9a5837475a39120d2350fc13f26b9c1b176f56a0
Matvey Bubalo <matveybu@Matveys-MacBook-Pro.local>
2026-03-23 13:31:01 -0600

    added the MetricFactory

--- src/main/java/spotifyanalytics/metrics/MetricFactory.java
+++ src/main/java/spotifyanalytics/metrics/MetricFactory.java
@@ -1,4 +1,14 @@
 package spotifyanalytics.metrics;
 
+import java.util.List;
+
 public class MetricFactory {
+    private static final int DEFAULT_LIMIT = 5;
+    public static List<ListeningMetric<?>> createDefaultMetrics(){
+        return List.of(
+                new TopArtistsMetric(DEFAULT_LIMIT),
+                new TopTracksMetric(DEFAULT_LIMIT),
+                new TimeListenedMetric()
+        );
+    }
 }

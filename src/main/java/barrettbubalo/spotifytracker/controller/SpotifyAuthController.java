214546b94b27fd7fe574f513cd260ce87302d059
jaronbarrettCU <157172045+jaronbarrettCU@users.noreply.github.com>
2026-04-29 02:24:18 -0600

    adding back missing imports in SpotifyAuthController

--- src/main/java/barrettbubalo/spotifytracker/controller/SpotifyAuthController.java
+++ src/main/java/barrettbubalo/spotifytracker/controller/SpotifyAuthController.java
@@ -14,6 +14,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+
+import jakarta.servlet.http.HttpSession;
 
 import org.springframework.http.ResponseEntity;
 
